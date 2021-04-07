4d5e48a0481e821cb94e1691a26a084cfc930844
pant <nptytn2@gmail.com>
2021-04-07 12:51:48 +0700

    Auto find moonbeam path before running setup systemd script

--- setup-systemd-service.sh
+++ setup-systemd-service.sh
@@ -5,7 +5,7 @@ echo "Setting up systemd service..."
 sudo adduser moonbase_service --system --no-create-home
 sudo mkdir /var/lib/alphanet-data
 sudo chown moonbase_service /var/lib/alphanet-data
-sudo cp ./target/release/moonbeam /var/lib/alphanet-data
+sudo cp $(find /home -type d -name 'moonbeam' | head -n 1)/target/release/moonbeam /var/lib/alphanet-data
 sudo curl https://raw.githubusercontent.com/npty/moonbeam-node-script/master/moonbeam.service --output /etc/systemd/system/moonbeam.service
 
 echo "Moonbeam systemd service has been setup successfully."
