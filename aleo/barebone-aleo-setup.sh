711cfe6f53fb6fd94ba56f29fdf684b039cd385f
pant <nptytn2@gmail.com>
2021-04-21 01:07:31 +0700

    Fix source script

--- aleo/barebone-aleo-setup.sh
+++ aleo/barebone-aleo-setup.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y git make build-essential cmake libssl-dev pkg-config
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-source ~/.profile
+source $HOME/.cargo/env
 
 # Install snarkos
 cargo install snarkos
