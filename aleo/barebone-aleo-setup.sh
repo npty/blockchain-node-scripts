84bc62660695b0a4a4661a3de624bc397c6e25cd
pant <nptytn2@gmail.com>
2021-04-21 00:57:32 +0700

    Add libssl-dev

--- aleo/barebone-aleo-setup.sh
+++ aleo/barebone-aleo-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install -y git make build-essential cmake
+sudo apt-get install -y git make build-essential cmake libssl-dev
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
