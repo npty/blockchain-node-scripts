ee727874a0cbe4a36b69989713508a573a06200d
pant <nptytn2@gmail.com>
2021-04-21 01:56:20 +0700

    Fix binary location

--- aleo/setup-systemd-service.sh
+++ aleo/setup-systemd-service.sh
@@ -5,6 +5,7 @@ echo "Setting up systemd service..."
 sudo adduser aleo_service --system --no-create-home 2>/dev/null
 sudo mkdir -p /var/lib/aleo-data
 sudo chown -R aleo_service /var/lib/aleo-data
+sudo cp $HOME/.cargo/bin/snarkos /var/lib/spacemesh-data
 sudo curl https://raw.githubusercontent.com/npty/blockchain-node-scripts/aleo/aleo/aleo.service --output /etc/systemd/system/aleo.service
 
 echo "Aleo systemd service has been setup successfully."
