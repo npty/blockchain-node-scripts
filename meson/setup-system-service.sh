168a48ed278d20f34081c8704bf791fcfdf57435
B6111427 <54477794+B6111427@users.noreply.github.com>
2021-04-21 04:51:16 +0700

    edit meson script

--- meson/setup-system-service.sh
+++ meson/setup-system-service.sh
@@ -9,8 +9,6 @@ sudo mkdir -p /var/lib/meson-data
 sudo mkdir -p /etc/meson
 echo "Select port=$port" | sudo tee /etc/meson/meson-service.env
 sudo chown -R meson_service /var/lib/meson-data
-sudo cp ~/meson-linux-amd64 /var/lib/meson-data
-#sudo curl https://discover.spacemesh.io/conf/28/config.json --output /var/lib/spacemesh-data/config.json
 sudo curl https://raw.githubusercontent.com/B6111427/blockchain-node-scripts/master/meson/meson.service --output /etc/systemd/system/meson.service
 
 echo "Meson systemd service has been setup successfully."
