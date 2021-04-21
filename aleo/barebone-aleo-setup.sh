2ebc9d19f0e46112c700a1965a24b8c4b19e6f4f
pant <nptytn2@gmail.com>
2021-04-21 23:20:22 +0700

    Update service file

--- aleo/barebone-aleo-setup.sh
+++ aleo/barebone-aleo-setup.sh
@@ -7,4 +7,12 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 # Install snarkos
-cargo install snarkos
+cd work
+git clone https://github.com/AleoHQ/snarkOS
+cd snarkOS
+latest_tag=$(git describe --tags)
+echo "Checkout version $latest_tag"
+git checkout ${latest_tag}
+
+# Build binary
+cargo build --release --verbose
