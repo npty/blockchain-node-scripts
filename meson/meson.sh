50241ae03ccfc61709a90801279f27c4689a7baa
B6111427 <54477794+B6111427@users.noreply.github.com>
2021-04-22 02:27:54 +0700

    sadas

--- meson/meson.sh
+++ meson/meson.sh
@@ -1,11 +1,9 @@
-sudo adduser meson_service --system --no-create-home 2>/dev/null
-sudo mkdir -p /var/lib/meson-data
-sudo cd /var/lib/meson-data
+
 sudo apt-get install -y wget
 sudo wget 'https://assets.meson.network:10443/static/terminal/v2.0.2/meson-linux-amd64.tar.gz'
 sudo tar -zxf meson-linux-amd64.tar.gz
-sudo rm -rf /var/lib/spacemesh-data/meson-linux-amd64/config.txt
-sudo curl https://raw.githubusercontent.com/B6111427/blockchain-node-scripts/master/meson/config.txt --output /var/lib/meson-data/meson-linux-amd64/config.txt
+sudo rm -rf ~/meson-linux-amd64/config.txt
+sudo curl https://raw.githubusercontent.com/B6111427/blockchain-node-scripts/master/meson/config.txt --output ~/meson-linux-amd64/config.txt
 sudo curl https://raw.githubusercontent.com/B6111427/blockchain-node-scripts/master/meson/meson.service --output /etc/systemd/system/meson.service
 sudo systemctl enable meson.service
 sudo systemctl start meson.service
