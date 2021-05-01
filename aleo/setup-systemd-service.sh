5468bc3e7a74e06ff8907735c160cd397af636d5
pant <nptytn2@gmail.com>
2021-05-01 08:14:24 +0700

    Fix ownership

--- aleo/setup-systemd-service.sh
+++ aleo/setup-systemd-service.sh
@@ -4,9 +4,11 @@ echo "Setting up systemd service..."
 
 miner_address=$1
 
+sudo adduser aleo_service --system
 sudo mkdir -p /var/lib/aleo-data
 sudo mkdir -p /etc/aleo
 sudo cp $HOME/work/snarkOS/target/release/snarkos /var/lib/aleo-data
+sudo chown aleo_service /var/lib/aleo-data
 
 if [ -z '$miner_address']; then
   sudo curl https://raw.githubusercontent.com/npty/blockchain-node-scripts/aleo/aleo/aleo-without-mining.service --output /etc/systemd/system/aleo.service
