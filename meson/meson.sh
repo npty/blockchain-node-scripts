696f14e58bd0df6ada481288ab74f0b1ede07cfb
B6111427 <54477794+B6111427@users.noreply.github.com>
2021-05-31 00:10:53 +0700

    Meson 2.5.1 extend space

--- meson/meson.sh
+++ meson/meson.sh
@@ -8,6 +8,6 @@ sudo tar -zxf meson-linux-amd64.tar.gz
 #sudo systemctl enable meson.service
 #sudo systemctl start meson.service
 cd ./meson-linux-amd64
-printf 'nmJoGxv8c5wr9ueTDykzUQ==\n12000\n400' | sudo ./meson service-install
+printf 'nmJoGxv8c5wr9ueTDykzUQ==\n12000\n420' | sudo ./meson service-install
 sudo ./meson service-start
 sudo watch systemctl status meson.service
