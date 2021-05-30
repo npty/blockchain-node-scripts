9e96c547e6ccd15b70b827c040e44ae33c70cae4
B6111427 <54477794+B6111427@users.noreply.github.com>
2021-05-30 23:55:11 +0700

    Meson 2.5.1

--- meson/meson.sh
+++ meson/meson.sh
@@ -1,6 +1,6 @@
 
 sudo apt-get install -y wget
-sudo wget 'https://assets.meson.network:10443/static/terminal/v2.5.0/meson-linux-amd64.tar.gz'
+sudo wget 'https://assets.meson.network:10443/static/terminal/v2.5.1/meson-linux-amd64.tar.gz'
 sudo tar -zxf meson-linux-amd64.tar.gz
 #sudo rm -rf ~/meson-linux-amd64/config.txt
 #sudo curl https://raw.githubusercontent.com/B6111427/blockchain-node-scripts/master/meson/config.txt --output ~/meson-linux-amd64/config.txt
@@ -8,6 +8,6 @@ sudo tar -zxf meson-linux-amd64.tar.gz
 #sudo systemctl enable meson.service
 #sudo systemctl start meson.service
 cd ./meson-linux-amd64
-printf 'nmJoGxv8c5wr9ueTDykzUQ==\n12000\n390' | sudo ./meson install
-sudo ./meson start
+printf 'nmJoGxv8c5wr9ueTDykzUQ==\n12000\n400' | sudo ./meson service-install
+sudo ./meson service-start
 sudo watch systemctl status meson.service
