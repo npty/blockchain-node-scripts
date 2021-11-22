127dc61ac0061330ed64f093a4f2e2c1a6ce5964
B6111427 <suphachaiphetthamrong@gmail.com>
2021-11-23 03:23:32 +0700

    aleo ceremony

--- aleo/aleo.sh
+++ aleo/aleo.sh
@@ -22,12 +22,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 #install tmux
 sudo apt-get install tmux
 
-tmux
-
-sudo docker run -it --rm --name setupcontrib --env COORD_URL=https://inner-ceremony.aleo.org aleohq/setup-contributor:latest
 
