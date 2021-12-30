fe1d88c9f71f2eabfc242ffd95a1db1db648c379
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2021-12-31 03:55:03 +0700

    Change to v2.5.2

--- meson/meson.sh
+++ meson/meson.sh
@@ -1,6 +1,6 @@
 
 sudo apt-get install -y wget
-sudo wget 'https://coldcdn.com/api/cdn/f2cobx/terminal/v2.5.1/meson-linux-amd64.tar.gz'
+sudo wget 'https://coldcdn.com/api/cdn/f2cobx/terminal/v2.5.2/meson-linux-amd64.tar.gz'
 sudo tar -zxf meson-linux-amd64.tar.gz
 #sudo rm -rf ~/meson-linux-amd64/config.txt
 #sudo curl https://raw.githubusercontent.com/B6111427/blockchain-node-scripts/master/meson/config.txt --output ~/meson-linux-amd64/config.txt
